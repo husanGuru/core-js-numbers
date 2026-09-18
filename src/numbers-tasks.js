08feb26ec6cccbc315f35be1357d752bd31ba0b1
husanGuru <abdigafurovhusan@gmail.com>
2026-09-19 02:07:46 +0500

    fix: lint

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -107,7 +107,7 @@ function getLinearEquationRoot(a, b) {
 function getAngleBetweenVectors(x1, y1, x2, y2) {
   return Math.acos(
     (x1 * x2 + y1 * y2) /
-    (Math.sqrt(x1 ** 2 + y1 ** 2) * Math.sqrt(x2 ** 2 + y2 ** 2))
+      (Math.sqrt(x1 ** 2 + y1 ** 2) * Math.sqrt(x2 ** 2 + y2 ** 2))
   );
 }
 
